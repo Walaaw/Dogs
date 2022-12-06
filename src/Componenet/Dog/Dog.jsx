fca07d0c22ca67bff58d10cdaf56a1bef031cde0
walaaMahmoud <walaamahmoud474@gmail.com>
2022-12-06 13:41:53 +0200

    update

--- src/Componenet/Dog/Dog.jsx
+++ src/Componenet/Dog/Dog.jsx
@@ -7,7 +7,7 @@ export default function Dog({dog,item}) {
     <div className={`bg-white shadow-sm rounded p-2 ${dogstyle.box} `}>
     
      <figure>
-     <Link to={`/details/${dog.name}`}> <img src={item?`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`:dog.image.url} alt="dog"  className={`w-100 rounded ${dogstyle.dog}`}/> </Link>
+     <Link to={item?`/details/${item.name}`:`/details/${dog.name}`}> <img src={item?`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`:dog.image.url} alt="dog"  className={`w-100 rounded ${dogstyle.dog}`}/> </Link>
         <figcaption >
             <p className={`fw-bold p-2 pb-0 fs-5 ${dogstyle.dogname}`}>{item?item.name:dog.name}</p>
             <p className={`px-2 pb-2 ${dogstyle.title}`}>{item?`Bred For : ${item.bred_for}`:`Bred For : ${dog.bred_for}`}</p>
